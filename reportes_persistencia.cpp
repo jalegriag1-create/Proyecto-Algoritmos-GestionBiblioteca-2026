c090e389f6bcfed7b82a7fb815195ac8a3cf7062
Eddy <ecastilloa15@gmail.com.edu.gt>
2026-09-13 16:49:53 -0600

    Cambio de .py a .cpp

---
+++ reportes_persistencia.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+// --- 1. GENERAR ESTADÍSTICAS ---
+
+
+void generarReportePrestamos(std::string fechaInicio, std::string fechaFin) {
+  
+}
+
+
+void obtenerLibrosMasSolicitados(int limiteCantidad) {
+   
+}
+
+
+// --- 2. GUARDAR Y CARGAR ARCHIVOS  ---
+
+bool guardarDatos(std::string nombreArchivo, std::string datos) {
+    
+    return true;
+}
+
+
+std::string cargarDatos(std::string nombreArchivo) {
+   
+    return "";
+}
+
+
+// --- 3. INTEGRAR CONSULTAS ---
+
+void filtrarRegistros(std::string criterio, std::string valor) {
+    
+}
